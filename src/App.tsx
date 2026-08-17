cc019a22466496abbca5859467e08314939362c3
timbererickson <timber.erickson@mesacounty.us>
2026-08-17 14:17:01 -0600

    Simplify App component by removing routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
-import { Routes, Route, Navigate } from 'react-router-dom'
 import DevMetricsDashboard from './pages/DevMetricsDashboard'
 
 export default function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<DevMetricsDashboard />} />
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
-  )
+  return <DevMetricsDashboard />
 }
